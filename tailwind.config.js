01570011de5c90e3cbffed6aa482421adca6cfd0
Alex Geer <monoflash@gmail.com>
2025-01-15 18:10:50 +0300

    Обновление базового шаблона.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     './src/js/**/*.js',
   ],
   theme: {
+    screens: {
+      'sl': '360px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
   plugins: [],
